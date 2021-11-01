65550fc3e6775e26656653562df764268dcdba6c
Omer <teouysal@outlook.com>
2021-11-01 17:58:47 +0300

    all business logic is done

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
 const express = require('express');
 const app = express();
-require('dotenv/config');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const mongoose = require('mongoose')
-const cors = require('cors')
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
+
+
+app.use(cors());
+app.options('*', cors())
+
+//middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(authJwt());
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+app.use(errorHandler);
+
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 
 const api = process.env.API_URL;
 
-app.use(cors())
-app.options('*',cors())
-app.use(bodyParser.json());
-app.use(morgan('tiny'))
-
-
-
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+app.use(`/welcome`, (req,res,next)=>{
+    res.status(200).json({
+        success:'success'
+    })
+});
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser : true,
     useUnifiedTopology : true,
     dbName : 'ecommerce-database'
 })
 .then(()=>{
-    console.log('we connected mong0')
+    console.log('we connected mongo')
 })
 .catch((err)=>{
     console.log(err)
@@ -30,6 +50,5 @@ mongoose.connect(process.env.CONNECTION_STRING, {
 
 
 app.listen(3000, ()=>{
-    console.log(api);
     console.log("server is running");
 })
